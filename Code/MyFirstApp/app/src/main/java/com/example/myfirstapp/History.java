60b7ff634206a57175a89ad41dfa2deba2822dcf
DESKTOP-5HC9V66\robsh <robshrives@gmail.com>
2016-10-28 21:44:12 -0400

    Retrying commit

--- Code/MyFirstApp/app/src/main/java/com/example/myfirstapp/History.java
+++ Code/MyFirstApp/app/src/main/java/com/example/myfirstapp/History.java
@@ -1,8 +1,43 @@
 package com.example.myfirstapp;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by robsh on 2016-10-27.
  */
 
 public class History {
+    Integer duration;
+    Integer interval;
+    String media[] = new String[4];
+
+    public History(Integer duration, Integer interval, String[] media) {
+        this.duration = duration;
+        this.interval = interval;
+        this.media = media;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public Integer getInterval() {
+        return interval;
+    }
+
+    public void setInterval(Integer interval) {
+        this.interval = interval;
+    }
+
+    public String[] getMedia() {
+        return media;
+    }
+
+    public void setMedia(String[] media) {
+        this.media = media;
+    }
 }
--- Code/MyFirstApp/app/src/main/java/com/example/myfirstapp/HistoryActivity.java
+++ Code/MyFirstApp/app/src/main/java/com/example/myfirstapp/HistoryActivity.java
@@ -2,6 +2,9 @@ package com.example.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.text.DateFormat;
+import java.util.Date;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        String currentDateTimeString = DateFormat.getDateInstance().format(new Date());
+
+        // textView is the TextView view that should display it
+        TextView currDate = (TextView) findViewById(R.id.currentDate);
+        currDate.setText(currentDateTimeString);
+
+//        int receiveValue = getIntent().getIntExtra("duration", 0);
+//        TextView duration = (TextView) findViewById(R.id.historyDuration);
+//        duration.setText(receiveValue);
     }
 }
